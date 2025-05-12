ff54af636c9aa17885345e363e9452f2e0e89e51
soncomqiq <idgamescs@hotmail.com>
2025-05-12 14:55:59 +0700

    Removed unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import Footer from "@/components/layout/Footer";
 import PropertiesList from "../components/properties/PropertiesList";
 import StatusBar from "@/components/layout/StatusBar";
